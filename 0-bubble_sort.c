a8099852b792cd08025b9247fbe72888df6522c3
“Darkidd77” <m.almahdi77@yahoo.com>
2024-02-20 22:45:07 +0000

    task- bubble-fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,24 +1,25 @@
 #include "sort.h"
 
 /**
- * bubble_sort - fun. to sort an array of ints
- * bubble sort algorithm
+ * bubble_sort - Function to sort an array of ints using the bubble sort algorithm
  *
- * @array: array with ints value
- * @size: array size
+ * @array: Array with ints values
+ * @size: Array size
  */
 
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
 	size_t x, j;
+	int s;
 
-	if (!array || !size)
+	if (!array || size <= 1)
 		return;
 
 	x = 0;
 	while (x < size)
 	{
+		s = 0;
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -27,8 +28,12 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				print_array(array, size);
+				s = 1;
 			}
 		}
 		x++;
+
+		if (!s)
+			break;
 	}
 }
